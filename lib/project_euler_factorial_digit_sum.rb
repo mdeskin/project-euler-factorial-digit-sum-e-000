434d34bd7531cdfafeec75be9f00f30ff709f433
Mike Deskin <michaelhaydendeskin@gmail.com>
2015-11-16 03:23:04 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,27 @@
-# code your solution here
+def factorial(n)
+  count = n
+  product = 1
+  until count == 0
+    product *= count
+    count -= 1
+  end
+  product
+end
+
+def sum_of_digits(n)
+  number_passed_str = n.to_s
+  count = 0
+  sum = 0
+   until number_passed_str.slice(count).nil?
+      sum = sum + number_passed_str.slice(count).to_i
+      count += 1
+   end
+  sum
+end
+
+def factorial_digit_sum(n)
+
+# product - number to get sum of digits
+sum_of_digits(factorial(n))
+
+end
